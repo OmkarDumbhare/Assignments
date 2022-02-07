1f76be1539e5c53513e6c4866e2c15a835dedf3f
OmkarDumbhare <97183988+OmkarDumbhare@users.noreply.github.com>
2022-02-07 10:30:52 +0530

    Add files via upload

---
+++ Core Java/Lambda Assignment/Assignment4Q3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.function.Function;
+
+public class Assignment4Q3 {
+    static int modifyValue(int n, Function fun) {
+        return (int)fun.apply(n);
+    }
+    static class Product {
+    }
+
+    static void display(int r) {
+        System.out.println(r);
+    }
+
+    public static void main(String[] args) {
+        int r =modifyValue(5,val->(Integer)val+ 5);
+        display(r);
+    }
+
+}
