0f94c6ce52679af665ea4c54974c543ea4835240
OmkarDumbhare <97183988+OmkarDumbhare@users.noreply.github.com>
2022-03-07 15:52:56 +0530

    Delete Spring Security/Assignment-SpringSecurity-Q5 directory

--- Spring Security/Assignment-SpringSecurity-Q5/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-Insert into users(username, password, enabled) values('user','upass','true');
-Insert into users(username, password, enabled) values('admin','apass','true');
-
-
-Insert into authorities(username, authority) values('user','ROLE_USER');
-Insert into authorities(username, authority) values('admin','ROLE_ADMIN');
